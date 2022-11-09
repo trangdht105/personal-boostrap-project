53ff1dd110ac21c35fad3448503bfd486f19f504
trangdht105 <108358250+trangdht105@users.noreply.github.com>
2022-11-09 17:21:53 +0700

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,54 @@
+// JavaScript Document
+$('#sp-gold').owlCarousel({
+    loop:true,
+    margin:30,
+    nav:true,
+	dots:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:3
+        }
+    }
+})
+
+$('#sp-gold-pro').owlCarousel({
+    loop:true,
+    margin:30,
+    nav:true,
+	dots:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:3
+        }
+    }
+})
+
+$('#sp-other').owlCarousel({
+    loop:true,
+    margin:30,
+    nav:true,
+	dots:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:3
+        }
+    }
+})
